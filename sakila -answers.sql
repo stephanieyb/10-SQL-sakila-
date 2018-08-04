4579b3e0d0fb63d1b1664d76afcfe6a7bc1312e2
stephanieyb <38571010+stephanieyb@users.noreply.github.com>
2018-08-04 13:45:59 -0400

    Update sakila -answers.sql

--- sakila -answers.sql
+++ sakila -answers.sql
@@ -1,35 +1,36 @@
--- 1a isplay the first and last names of all actors from the table `actor`.
+-- 1a Display the first and last names of all actors from the table `actor`.
 SELECT first_name,last_name FROM actor;
--- 1b
+-- 1b Display the first and last name of each actor in a single column in upper case letters. Name the column `Actor Name`.
 SELECT CONCAT(first_name,' ',last_name) AS Actor_Name FROM actor;
--- 2a
+-- 2a Find the ID#, first name, and last name of an actor, of whom you know only the first name, "Joe." 
 SELECT * FROM actor WHERE first_name LIKE 'Joe%'; 
--- 2b
+-- 2b Find all actors whose last name contain the letters `GEN`:
 SELECT * FROM actor WHERE last_name LIKE 'GEN%';
--- 2c
+-- 2c Find all actors whose last names contain the letters `LI`. This time, order the rows by last name 
 SELECT * FROM actor WHERE last_name LIKE '%LI%' order by last_name;
--- 2d
+-- 2d Using `IN`, display the `country_id` and `country` columns of the following countries: Afghanistan, Bangladesh, and China
 SELECT * FROM Country WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
--- 3a
+-- 3a Add a `middle_name` column to the table `actor`. Position it between `first_name` and `last_name`
+Alter Table actor
+ADD middle_name varchar(30) AFTER first_name;
+-- 3b You realize that some of these actors have tremendously long last names. Change the data type of the `middle_name` column to `blobs`.
+ALTER TABLE actor 
+MODIFY COLUMN middle_name BLOB;
+-- 3c Now delete the middle_name column.
 ALTER TABLE actor
-ADD description BLOB;
--- 3b 
-ALTER TABLE actor
-DROP COLUMN description;
--- 4a
-SELECT last_name FROM actor;
---  4b
-SELECT last_name, COUNT(last_name) counts
+DROP COLUMN middle_name;
+-- 4a List the last names of actors, as well as how many actors have that last name.
+SELECT last_name, COUNT(last_name) AS counts
 FROM actor
 GROUP BY last_name
 ORDER BY COUNT(last_name) DESC;
---
+-- 4b List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
 SELECT last_name, COUNT(last_name) counts
 FROM actor
 GROUP BY last_name
 HAVING COUNT(last_name) >=2
 ORDER BY COUNT(last_name);
---
+-- 4c The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`
 SELECT * FROM actor;
 UPDATE actor
 SET first_name = 'HARPO' 
@@ -44,7 +45,7 @@ CASE
 END
 WHERE actor_id = 172;
 
--- 5a
+-- 5a You cannot locate the schema of the `address` table. Which query would you use to re-create it?
 SHOW CREATE TABLE sakila.address;
 -- 6a
 SELECT first_name, last_name, address FROM staff 
